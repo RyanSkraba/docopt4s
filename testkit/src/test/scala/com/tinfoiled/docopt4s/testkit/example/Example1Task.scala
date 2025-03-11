3350efc52815e083f715a9135b62ccf431da65f5
Ryan Skraba <ryan@skraba.com>
2025-03-11 08:15:45 +0100

    test: Add examples for getting options

--- testkit/src/test/scala/com/tinfoiled/docopt4s/testkit/example/Example1Task.scala
+++ testkit/src/test/scala/com/tinfoiled/docopt4s/testkit/example/Example1Task.scala
@@ -13,24 +13,44 @@ object Example1Task extends Task {
     s"""$Description
        |
        |Usage:
-       |  ${ExampleGo.Name} $Cmd ARG1 [ARG2] [ARG3...] [--flag]
+       |  ${ExampleGo.Name} $Cmd [--default DEFAULT|--options] ARG1 [ARG2] [ARG3...]
        |
        |Options:
-       |  -h --help  Show this screen.
-       |  --version  Show version.
-       |  ARG1       Mandatory argument
-       |  ARG2       Optional argument [Default: 0]
-       |  ARG3       Repeated argument
-       |  --flag     A flag argument
+       |  -h --help          Show this screen.
+       |  --version          Show version.
+       |  --default=DEFAULT  If present, use as the default value for string arguments.
+       |  --options          If present, get the arguments as options.
+       |  ARG1               Mandatory argument.
+       |  ARG2               Optional integer argument.
+       |  ARG3               Repeated argument.
        |
        |Some description of the command line interface task
        |""".stripMargin.trim
 
   def go(opts: TaskOptions): Unit = {
-    println(s"Command: $Cmd")
-    println(s"ARG1: ${opts.getString("ARG1")}")
-    println(s"ARG2: ${opts.getStringOption("ARG2")}")
-    println(s"ARG3: ${opts.getStrings("ARG3")}")
-    println(s"--flag: ${opts.getBoolean("--flag")}")
+    println(s"Command:$Cmd")
+    if (opts.getBoolean("--options")) {
+      print(s"""--options:${opts.getBooleanOption("--options")}
+           |--default:${opts.getStringOption("--default")}
+           |ARG1:${opts.getStringOption("ARG1")}
+           |ARG2:${opts.getIntOption("ARG2")}
+           |ARG3:${opts.getStringsOption("ARG3").map(_.mkString("(", ",", ")"))}
+           |""".stripMargin)
+    } else if (opts.getStringOption("--default").nonEmpty) {
+      val dflt = opts.getString("--default")
+      print(s"""--options:${opts.getBoolean("--options")}
+           |--default:$dflt}
+           |ARG1:${opts.getString("ARG1", dflt)}
+           |ARG2:${opts.getInt("ARG2", -1)}
+           |ARG3:${opts.getStrings("ARG3", Seq(dflt)).map(_.mkString("(", ",", ")"))}
+           |""".stripMargin)
+    } else {
+      print(s"""--options:${opts.getBoolean("--options")}
+           |--default:${opts.getStringOption("--default")}
+           |ARG1:${opts.getString("ARG1")}
+           |ARG2:${opts.getInt("ARG2", default = 0)}
+           |ARG3:${opts.getStrings("ARG3").mkString("(", ",", ")")}
+           |""".stripMargin)
+    }
   }
 }
--- testkit/src/test/scala/com/tinfoiled/docopt4s/testkit/example/Example1TaskSpec.scala
+++ testkit/src/test/scala/com/tinfoiled/docopt4s/testkit/example/Example1TaskSpec.scala
@@ -5,10 +5,11 @@ import com.tinfoiled.docopt4s.testkit.MultiTaskMainSpec
 /** Unit tests for [[Example1Task]] */
 class Example1TaskSpec extends MultiTaskMainSpec(ExampleGo, Some(Example1Task)) {
 
-  describe(s"${Main.Name} $TaskCmd command line") {
+  describe(s"${Main.Name} $TaskCmd command line errors") {
     itShouldThrowOnHelpAndVersionFlags()
     itShouldThrowOnUnknownFlag()
     itShouldThrowOnMissingOpt(Seq.empty)
+    itShouldThrowOnMissingOptValue(Seq("--default"))
   }
 
   describe("When running with arguments") {
@@ -16,11 +17,12 @@ class Example1TaskSpec extends MultiTaskMainSpec(ExampleGo, Some(Example1Task)) 
       withGoMatching(TaskCmd, "arg1") { case (stdout, stderr) =>
         stderr shouldBe empty
         stdout shouldBe
-          """Command: example1
-            |ARG1: arg1
-            |ARG2: None
-            |ARG3: Iterable()
-            |--flag: false
+          """Command:example1
+            |--options:false
+            |--default:None
+            |ARG1:arg1
+            |ARG2:0
+            |ARG3:()
             |""".stripMargin
       }
     }

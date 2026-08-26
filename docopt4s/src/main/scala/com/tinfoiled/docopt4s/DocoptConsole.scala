272c0be6e55da365d141a374817c240f86501e17
Ryan Skraba <ryan@skraba.com>
2026-08-26 07:55:15 +0200

    feat: Add a DocoptConsole that configures from flags

---
+++ docopt4s/src/main/scala/com/tinfoiled/docopt4s/DocoptConsole.scala
@@ -0,0 +1,30 @@
+package com.tinfoiled.docopt4s
+
+/** Create a console using the standard flags from the Docopt
+  * @param opt
+  *   The options keys and values that are currently set
+  * @param verboseFlag
+  *   The flag, if present, turns on the verbose mode.
+  * @param noVerboseFlag
+  *   The flag, if present, turns on the verbose mode.
+  * @param plainFlag
+  *   The flag, if present, turns on the plain mode.
+  * @param yesFlag
+  *   The flag, if present, turns on the yes mode.
+  */
+case class DocoptConsole(
+    opt: Docopt,
+    verboseFlag: String = "--verbose",
+    noVerboseFlag: String = "--noVerbose",
+    plainFlag: String = "--quiet",
+    yesFlag: String = "--yes"
+) extends AnsiConsole {
+  if (opt.flag(verboseFlag) && opt.flag(noVerboseFlag))
+    throw new IncompatibleKeysException(verboseFlag, noVerboseFlag)
+
+  override protected lazy val cfg: AnsiConsole.Cfg = AnsiConsole.Cfg(
+    verbose = opt.flag(verboseFlag) || !opt.flag(noVerboseFlag),
+    plain = opt.flag(plainFlag),
+    yes = opt.flag(yesFlag)
+  )
+}
